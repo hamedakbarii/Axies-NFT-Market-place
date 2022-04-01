2af1fa332e664592e2ecaaeaf970117ba014983f
Hamed Akbari <hamedkarate0@gmail.com>
2022-04-01 21:11:44 +0430

    login page created

--- script/script.js
+++ script/script.js
@@ -6,3 +6,22 @@ let input = document.querySelector('.search_input')
 search_btn.addEventListener('click', ()=> {
     input.classList.toggle('search_input--show')
 })
+
+
+// login page 
+const Einput = document.querySelector('.einput')
+const Pinput = document.querySelector('.pinput')
+const submitButton = document.querySelector('.btn')
+
+
+function sbmitfunction() {
+    if (Einput.value && Pinput.value) {
+        alert('Welcome! you signed in')
+    }
+    else {
+        alert('something went wrong try again later or creat a new account')
+    }
+}
+
+
+submitButton.addEventListener('click' , sbmitfunction)
