563c2a7ce13a7d9a49765b77e93bb9bcfd73425d
Hamed Akbari <hamedkarate0@gmail.com>
2022-04-01 19:10:33 +0430

    sticky added - navbar updated - search box updated -

---
+++ script/script.js
@@ -0,0 +1,8 @@
+// search icon navbar
+
+const search_btn = document.querySelector('.searchIcon')
+let input = document.querySelector('.search_input')
+
+search_btn.addEventListener('click', ()=> {
+    input.classList.toggle('search_input--show')
+})
