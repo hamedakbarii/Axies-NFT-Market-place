6066bdaa58478b4e1a523c675deb7a326dbb3611
Hamed Akbari <hamedkarate0@gmail.com>
2022-04-18 12:22:59 +0430

    favicon added

--- script/script.js
+++ script/script.js
@@ -1,14 +1,19 @@
-// search icon navbar
 
-const search_btn = document.querySelector('.searchIcon')
-let input = document.querySelector('.search_input')
+// Search icon navbar
 
-search_btn.addEventListener('click', ()=> {
-    input.classList.toggle('search_input--show')
+const search_btn = document.querySelector('.glass');
+let input = document.querySelector('.search_input');
+let deleteBtn = document.querySelector('.delete');
+
+search_btn.addEventListener('click',function(){
+    input.classList.toggle('search_input--show');
+    deleteBtn.style.display = ('block');
+    if (!input.classList.contains('search_input--show')) {
+        deleteBtn.style.display = ('none');
+    }
 })
 
-
-
-
-
-
+deleteBtn.addEventListener('click',function(){
+    input.classList.remove('search_input--show');
+    deleteBtn.style.display = ('none');
+})
