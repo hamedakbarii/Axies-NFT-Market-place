c7f7bff38d87d6a0e4d304611abbb1efcd9155e7
Hamed Akbari <hamedkarate0@gmail.com>
2022-04-18 20:13:08 +0430

    scroll up button added

--- script/script.js
+++ script/script.js
@@ -1,6 +1,4 @@
-
 // Search icon navbar
-
 const search_btn = document.querySelector('.glass');
 let input = document.querySelector('.search_input');
 let deleteBtn = document.querySelector('.delete');
@@ -17,3 +15,25 @@ deleteBtn.addEventListener('click',function(){
     input.classList.remove('search_input--show');
     deleteBtn.style.display = ('none');
 })
+
+// scroll up button
+mybutton = document.querySelector('.scroll__btn');
+
+
+window.onscroll = function () {
+    scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
